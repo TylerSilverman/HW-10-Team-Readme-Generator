b828e2085cdf5ce002db0c7d8e20e0dc35918586
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-14 11:45:53 -0500

    edited some info on the js tabs

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,14 +1,13 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor (name,id, email, getName, getId, getEmail, getRole, returnsEmployee){
+    constructor (name,id, email, getName, getId, getEmail, getRole){
         this.name = name;
         this.id = id;
         this.email = email;
         this.getName() = getName();
         this.getId() = getId();
         this.getEmail() = getEmail();
-        this.getRole() = getRole();
-        this.returnsEmployee = returnsEmployee;
+        this.getRole() = getRole(); // returns Employee
     };
 };
 
--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -2,7 +2,16 @@ const Employee = require("./Employee");
 
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 class Engineer extends Employee {
-    
+    constructor(){
+        const 
+
+        super(name, id, email)
+        this.officeNumber = this.officeNumber;
+        this.gerRole() //Override to return Manager
+    }
 }
+
+const engineer = new Engineer();
+engineer.printInfo ();
 
 module.exports = Engineer;
